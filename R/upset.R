68a024a41edcc2ef9c1ed83705ee8dd0edff04d2
Wang Xin <wangx555@mail2.sysu.edu.cn>
2019-05-05 14:51:51 +0800

    update upset.R

--- R/upset.R
+++ R/upset.R
@@ -17,51 +17,51 @@ library("cowplot")
 #' @export
 #'
 #' @examples
-#' result<-upsetanalysis(maf,TRUE)
+#' result<-upsetanalysis(maf.used_patient1,TRUE)
+
+
 
 maf<-read.table(Args[1],sep = "\t",header = TRUE)
 
 parameter1<-as.character(Args[2])
 
+for (i in 1:length(maf$Hugo_Symbol)){
+  maf$Patient[i]<-strsplit(as.character(maf$Tumor_Sample_Barcode[i]),split = "-")[[1]][1]
+}
+
+maf$Patient<-as.factor(maf$Patient)
+
+maf.used_patient1<-maf[which (maf$Patient==levels(maf$Patient)[1]),]
+
+primitive_length<-length(maf.used_patient1$Hugo_Symbol)
+
 
 upsetanalysis<-function(df,text_show=FALSE){
   
-#get the first patient's data
-  
-  for (i in 1:length(df$Hugo_Symbol)){
-    df$Patient[i]<-strsplit(as.character(df$Tumor_Sample_Barcode[i]),split = "-")[[1]][1]
-  }
-  
-  df$Patient<-as.factor(df$Patient)
-  
-  maf.used_patient1<-df[which (df$Patient==levels(df$Patient)[1]),]
-  
-  primitive_length<-length(maf.used_patient1$Hugo_Symbol)
 
-  
 #separate by quote
   
-  maf.used_patient1$Hugo_Symbol<-as.character(maf.used_patient1$Hugo_Symbol)
+  df$Hugo_Symbol<-as.character(df$Hugo_Symbol)
   new_gene<-c()
   
-  for (n in 1:length(maf.used_patient1$Hugo_Symbol)){
-    new_gene<-c(new_gene,na.omit(strsplit(maf.used_patient1$Hugo_Symbol[n],split = ",")[[1]][2:100]))
+  for (n in 1:length(df$Hugo_Symbol)){
+    new_gene<-c(new_gene,na.omit(strsplit(df$Hugo_Symbol[n],split = ",")[[1]][2:100]))
   }
   
   n=1
   while (n>0 & n<primitive_length+1){
-    gene_number_n<-as.numeric(length(strsplit(as.character(maf.used_patient1$Hugo_Symbol[n]),split = ",")[[1]]))
+    gene_number_n<-as.numeric(length(strsplit(as.character(df$Hugo_Symbol[n]),split = ",")[[1]]))
     
     if (gene_number_n>1){
-      maf.used_patient1[n,1]<-strsplit(maf.used_patient1$Hugo_Symbol[n],split = ",")[[1]][1]
+      df[n,1]<-strsplit(df$Hugo_Symbol[n],split = ",")[[1]][1]
       
       while (gene_number_n>1){
-        l=length(maf.used_patient1$Hugo_Symbol)
+        l=length(df$Hugo_Symbol)
         
-        maf.used_patient1$Hugo_Symbol<-as.character(maf.used_patient1$Hugo_Symbol)
+        df$Hugo_Symbol<-as.character(df$Hugo_Symbol)
         
-        maf.used_patient1[l+1,2:9]<-maf.used_patient1[n,2:9]
-        maf.used_patient1[l+1,1]<-as.character(strsplit(maf.used_patient1$Hugo_Symbol[n],split = ",")[[1]][gene_number_n])
+        df[l+1,2:9]<-df[n,2:9]
+        df[l+1,1]<-as.character(strsplit(df$Hugo_Symbol[n],split = ",")[[1]][gene_number_n])
         
         gene_number_n=gene_number_n-1
       }
@@ -70,33 +70,33 @@ upsetanalysis<-function(df,text_show=FALSE){
     n=n+1 
   }
   
-  new_gene_number<-length(maf.used_patient1$Hugo_Symbol)-primitive_length
+  new_gene_number<-length(df$Hugo_Symbol)-primitive_length
   
   for (n in 1:new_gene_number){
-    maf.used_patient1[primitive_length+n,1]<-new_gene[n]
+    df[primitive_length+n,1]<-new_gene[n]
   }
   
   #discrete the different data
   
-  for (n in 1:length(maf.used_patient1$Hugo_Symbol)){
+  for (n in 1:length(df$Hugo_Symbol)){
     
-    if (length(strsplit(as.character(maf.used_patient1[n,5]),split = "\t")[[1]])>2){
-      maf.used_patient1[n,]<-NA
+    if (length(strsplit(as.character(df[n,5]),split = "\t")[[1]])>2){
+      df[n,]<-NA
     }
     
   }
   
-  maf.used_patient1<-maf.used_patient1[,c(1:5,7,9,15,16)]
-  maf.used_patient1<-na.omit(maf.used_patient1)
+  df<-df[,c(1:5,7,9,15,16)]
+  df<-na.omit(df)
   
   
   #organize dataframe
   
-  maf.used_patient1$Tumor_Sample_Barcode<-factor(maf.used_patient1$Tumor_Sample_Barcode,levels = unique(maf.used_patient1$Tumor_Sample_Barcode))
+  df$Tumor_Sample_Barcode<-factor(df$Tumor_Sample_Barcode,levels = unique(df$Tumor_Sample_Barcode))
   
-  maf.used_patient1$Variant_Classification<-factor(maf.used_patient1$Variant_Classification,levels = unique(maf.used_patient1$Variant_Classification))
+  df$Variant_Classification<-factor(df$Variant_Classification,levels = unique(df$Variant_Classification))
   
-  maf.used_patient1<-maf.used_patient1[order(maf.used_patient1$Tumor_Sample_Barcode),]
+  df<-df[order(df$Tumor_Sample_Barcode),]
   
   
   #get the intersection genes
@@ -123,18 +123,18 @@ upsetanalysis<-function(df,text_show=FALSE){
     return(result)
   }
   
-  maf.used_patient1$Reference_Allele<-as.character(maf.used_patient1$Reference_Allele)
+  df$Reference_Allele<-as.character(df$Reference_Allele)
   
-  maf.used_patient1$Tumor_Seq_Allele2<-as.character(maf.used_patient1$Tumor_Seq_Allele2)
+  df$Tumor_Seq_Allele2<-as.character(df$Tumor_Seq_Allele2)
   
   
-  sample_combination<-combination(c(levels(maf.used_patient1$Tumor_Sample_Barcode)))
+  sample_combination<-combination(c(levels(df$Tumor_Sample_Barcode)))
   
-  for (n in 1:length(maf.used_patient1$Hugo_Symbol)){
-    maf.used_patient1[n,10]<-paste(c(as.character(maf.used_patient1[n,][c(1:4,6,7)])),collapse = "_")
+  for (n in 1:length(df$Hugo_Symbol)){
+    df[n,10]<-paste(c(as.character(df[n,][c(1:4,6,7)])),collapse = "_")
   }
   
-  colnames(maf.used_patient1)[10]<-c("needed_data")
+  colnames(df)[10]<-c("needed_data")
   
   
   #get intersection
@@ -142,6 +142,7 @@ upsetanalysis<-function(df,text_show=FALSE){
   all_intersect<-list()
   
   get_intersect<-function(sample,information){
+    
     combination_number<-length(sample)/length(sample[,1])
     sample_number<-length(sample[,1])
     
@@ -164,13 +165,14 @@ upsetanalysis<-function(df,text_show=FALSE){
     }
   }
   
-  process<-lapply(sample_combination,get_intersect,maf.used_patient1)
+  process<-lapply(sample_combination,get_intersect,df)
   
   
   #get the dataframe only includes genes 
   
   o<-1
   all_intersect_only<-list()
+  
   while(o>0 & o<length(all_intersect)+1){
     
     if (o%%2==0){
@@ -191,24 +193,31 @@ upsetanalysis<-function(df,text_show=FALSE){
   
   #get the first dataframe with one sample
   
-  sample_number<-length(levels(maf.used_patient1$Tumor_Sample_Barcode))
+  sample_number<-length(levels(df$Tumor_Sample_Barcode))
   
-  single_represent<-c(1:length(levels(maf.used_patient1$Tumor_Sample_Barcode)))
+  single_represent<-c(1:length(levels(df$Tumor_Sample_Barcode)))
   
   final_frame0<-data.frame(0,0,0)
+  
   colnames(final_frame0)<-c("Sample","Type","Number")
+  
   final_frame0<-final_frame0[-1,]
   
   n=1
   while(n>0 & n<sample_number+1){
+    
     sub_n<-setdiff(single_represent,single_represent[n])
+    
     seperate_n<-setdiff(unlist(all_intersect_only[n]),unlist(all_intersect_only[sub_n]))
+    
     final_seperate_n<-c()
     
     a=1
     while (a>0 & a<length(seperate_n)+1){
-      final_seperate_n<-c(final_seperate_n,as.character(unique(maf.used_patient1$Variant_Classification[which (maf.used_patient1$needed_data==seperate_n[a])])))
+      
+      final_seperate_n<-c(final_seperate_n,as.character(unique(df$Variant_Classification[which (df$needed_data==seperate_n[a])])))
       a=a+1
+      
     }
     
     try_seperate_n<-data.frame(0,0,0)
@@ -217,7 +226,9 @@ upsetanalysis<-function(df,text_show=FALSE){
     try_seperate_n[1:length(names(table(final_seperate_n))),2]<-names(table(final_seperate_n))
     try_seperate_n[1:length(names(table(final_seperate_n))),3]<-as.character(table(final_seperate_n))
     try_seperate_n$Sample<-sample_combination[[1]][n]
+    
     final_frame0<-rbind(final_frame0,try_seperate_n)
+    
     n=n+1
   }
   
@@ -236,10 +247,12 @@ upsetanalysis<-function(df,text_show=FALSE){
   
   sample_order_0<-sample_order_0[-1,]
   
-  sample_order_0[1:sample_number,1]<-levels(maf.used_patient1$Tumor_Sample_Barcode)[1:sample_number]
+  sample_order_0[1:sample_number,1]<-levels(df$Tumor_Sample_Barcode)[1:sample_number]
   
   for (n in 1:sample_number){
+    
     sample_order_0[n,2]<-sum(as.integer(final_frame0$Number[which(final_frame0$Sample==levels(final_frame0$Sample)[n])]))
+  
   }
   
   sample_order_0<-sample_order_0[order(sample_order_0$Number,decreasing =TRUE),]
@@ -298,7 +311,7 @@ upsetanalysis<-function(df,text_show=FALSE){
       types_n<-c()
       
       while (n>0 & n<(length(all_intersect_only[p][[1]])+1)){
-        types_n<-c(types_n,as.character(unique(maf.used_patient1$Variant_Classification[which (maf.used_patient1$needed_data==all_intersect_only[p][[1]][n])])))
+        types_n<-c(types_n,as.character(unique(df$Variant_Classification[which (df$needed_data==all_intersect_only[p][[1]][n])])))
         n=n+1
       }
       
@@ -312,7 +325,7 @@ upsetanalysis<-function(df,text_show=FALSE){
       
       try_n$Sample[1:length(names(table(types_n)))]<-paste(sample_combination[[q]][,(p-sum_CNM(sample_number,q-1))],collapse = "∩")
       
-      try_n$Type<-factor(try_n$Type,levels = levels(maf.used_patient1$Variant_Classification))
+      try_n$Type<-factor(try_n$Type,levels = levels(df$Variant_Classification))
       
       final_frame1<-rbind(final_frame1,try_n)
       p=p+1
@@ -368,6 +381,7 @@ upsetanalysis<-function(df,text_show=FALSE){
   row_numbers=1
   
   while (i>0 & i<length(levels(all_final_frame$Sample))+1){
+    
     sample_numbers_i<-length(strsplit(as.character(levels(all_final_frame$Sample)[i]),split = "∩")[[1]])
     
     point_line_frame[row_numbers:(row_numbers+sample_numbers_i-1),1]<-as.character(levels(all_final_frame$Sample)[i])
